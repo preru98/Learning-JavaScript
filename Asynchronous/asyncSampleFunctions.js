c84bd117bd5616e88d3cd9003a07e4f47c148696
Prerna <prerna1998.ps@gmail.com>
2021-02-28 08:42:26 +0530

    Callbacks | Error Handling

--- Asynchronous/asyncSampleFunctions.js
+++ Asynchronous/asyncSampleFunctions.js
@@ -1,29 +1,47 @@
 //Solution to functions executing in undesirable order using callbacks
 function download(downloaded){
     console.log('Downloading...');
+    //simulate error
+    //return downloaded(new Error('Download failed!'));
     setTimeout(()=> {
         console.info('Download complete');
         downloaded();
     }, 4000);
 };
+
 function compress (compressed){
     console.log('Compressing...');
+    //simulate error
+    return compressed(new Error('Compression failed!'));
     setTimeout(()=> {
-        console.info('Operation Successful');
+        console.info('Compression Successful');
         compressed();
     }, 2000);
 };
 
-function upload (){
+function upload (uploaded){
     console.log('Uploading...');
+    //simulate error    
+    return uploaded(new Error('Upload failed!'));
     setTimeout(()=> {
         console.info('Uploaded! Visit all files.');
     }, 6000);
 };
 
 //Executes in undesirable order.
-download(() => {
-    compress(() => {
-        upload();
+download((err) => {
+    if(err != null){
+        throw err;
+    }    compress((err) => {
+        if(err!=null){
+            throw err;
+        }
+        upload((err)=>{
+            if(err){
+                throw err;
+            }
+        });
     });
 });
+
+//Warn error ToDo
